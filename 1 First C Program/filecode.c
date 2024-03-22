8aa7945ef06c20d0db93a9bcb1662f37284506cf
Prashant Jain <prashant@knowledgegate.in>
2024-03-22 09:31:05 +0530

    First Chapter

---
+++ 1 First C Program/filecode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  printf("First code in file");
+  return 0;
+}
---
+++ 1 First C Program/print.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  printf("Hello World\n");
+
+  printf("My age \n is %d", 32);
+  return 0;
+}
